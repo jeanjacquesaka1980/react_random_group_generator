ce250beb905aa943304968b8f0734d8146f8787a
Jeremy Bertin <jeremy.bertin76@hotmail.fr>
2020-01-20 17:34:16 +0100

    corrected bug

---
+++ random-profile/precache-manifest.65ddfa4fa9878ab7b6d794d67c4ed799.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a167a94bd44c2f1e65da75ed1a14a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32ddf6ccb135706116cf",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "f94fc4cc29e9d73fcc5b",
+    "url": "/static/css/main.6039a524.chunk.css"
+  },
+  {
+    "revision": "32ddf6ccb135706116cf",
+    "url": "/static/js/2.1057c5b6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1057c5b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f94fc4cc29e9d73fcc5b",
+    "url": "/static/js/main.a51d7e2b.chunk.js"
+  },
+  {
+    "revision": "dc8ac5c89c7b1055b98c",
+    "url": "/static/js/runtime-main.af6fb007.js"
+  }
+]);
